7d001b522e1af8194d802c72f66af5af80a5402b
João Costa <joaocosta@joaos-mbp.lan>
2024-07-17 18:10:21 +0100

    added header

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,5 +1,14 @@
-import React from "react";
+import SubvisualIcon from "@/assets/icons/subvisual_icon";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="flex border-b border-dark_blue pt-10 pb-4">
+      <div className="ml-auto text-dark_blue items-center flex gap-4">
+        <span className="flex-1 text-[1.75rem] font-zilla_slab leading-10">
+          Powered by
+        </span>
+        <SubvisualIcon />
+      </div>
+    </header>
+  );
 }
