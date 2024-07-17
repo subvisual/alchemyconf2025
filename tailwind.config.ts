af0c7f36e4c8bba7ecb95c27dbf00ba3288e8c23
João Costa <joaocosta@joaos-mbp.lan>
2024-07-17 16:30:35 +0100

    tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,9 +11,12 @@ const config: Config = {
       background: "#FFF5DA",
       dark_blue: "#003049",
       sage: "#D7D9B1",
+      purple: "#827191",
+      bordeux: "#7D1D3F",
     },
     fontFamily: {
       zilla_slab: ["var(--font-zilla-slab)"],
+      koulen: ["var(--font-zilla-koulen)"],
     },
   },
   plugins: [],
