8e55f1b7842bf600fe713a7c18a33c4149085a40
João Costa <joaocosta@joaos-mbp.lan>
2024-07-17 16:37:36 +0100

    Added koulen font

--- src/utils/fonts.ts
+++ src/utils/fonts.ts
@@ -6,8 +6,8 @@ export const zilla_slab = Zilla_Slab({
   variable: "--font-zilla-slab",
 });
 
-export const koulen = Zilla_Slab({
+export const koulen = Koulen({
   subsets: ["latin"],
-  weight: ["400", "500", "700"],
+  weight: ["400"],
   variable: "--font-koulen",
 });
