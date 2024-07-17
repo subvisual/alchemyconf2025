5574a1c059e6c507d662e47b49782d42028a133c
João Costa <joaocosta@joaos-mbp.lan>
2024-07-17 16:30:01 +0100

    add new configs and logo

--- src/utils/fonts.ts
+++ src/utils/fonts.ts
@@ -1,7 +1,13 @@
-import { Zilla_Slab } from "next/font/google";
+import { Zilla_Slab, Koulen } from "next/font/google";
 
 export const zilla_slab = Zilla_Slab({
   subsets: ["latin"],
   weight: ["400", "500", "700"],
   variable: "--font-zilla-slab",
+});
+
+export const koulen = Zilla_Slab({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  variable: "--font-koulen",
 });
