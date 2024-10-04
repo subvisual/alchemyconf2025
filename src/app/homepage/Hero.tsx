f5631e04b76fc85e8dd84220671a743714ddd86c
josevazf <josevaz.fco@gmail.com>
2024-10-04 10:38:57 +0100

    Updated Hero section

--- src/app/homepage/Hero.tsx
+++ src/app/homepage/Hero.tsx
@@ -1,37 +1,38 @@
 import AlchemyIcon from "@/assets/icons/alchemy_icon";
-import ArrowDown from "@/assets/icons/arrow_down";
+// import ArrowDown from "@/assets/icons/arrow_down";
 
 export default function Hero() {
   return (
     <section className="flex min-h-[calc(100vh_+_80px)] flex-col items-center justify-center px-32">
-      <div className="flex justify-around gap-32">
+      <div className="flex justify-around gap-0">
         <div className="flex flex-col gap-[40px]">
           <AlchemyIcon className="h-[130px] w-[140px]" />
           <div>
-            <h1 className="text-dark-blue font-koulen text-[160px] leading-none">
+            <h1 className="font-koulen text-[200px] leading-[0.9] text-dark-blue">
               Alchemy <br /> Conf <br />{" "}
               <span className="text-bordeux">2025</span>
             </h1>
           </div>
         </div>
-        <div className="flex flex-col items-end justify-end gap-20 text-right">
-          <h2 className="font-zilla_slab text-[36px] font-semibold uppercase leading-none text-bordeux">
+        <div className="mb-6 flex flex-col items-end justify-end gap-16 text-right">
+          <h2 className="font-zilla_slab text-[32px] font-semibold uppercase leading-none text-bordeux">
             Talks, <br />
             Workshops, <br />
             Networking, <br />
-            And so much more!
+            And Celebrating Elixir!
           </h2>
-          <h3 className="text-dark-blue font-zilla_slab text-[36px] font-semibold uppercase leading-none">
-            2 - 3 April <br /> Braga, Portugal
+          <h3 className="leading-12 font-zilla_slab text-[32px] font-semibold uppercase text-dark-blue">
+            March 30-3 April, 2025 <br /> Braga, Portugal
           </h3>
         </div>
       </div>
-      <div className="flex flex-col items-center pt-8">
-        <a href="#tickets" className="font-zilla_slab text-[24px] uppercase">
-          Don&apos;t miss a beat!
-        </a>
-        <ArrowDown className="!h-3 animate-bounce" />
-      </div>
+      {/* NOTE: might need this section */}
+      {/* <div className="flex flex-col items-center pt-8"> */}
+      {/*   <a href="#tickets" className="font-zilla_slab text-[24px] uppercase"> */}
+      {/*     Don&apos;t miss a beat! */}
+      {/*   </a> */}
+      {/*   <ArrowDown className="!h-3 animate-bounce" /> */}
+      {/* </div> */}
     </section>
   );
 }
