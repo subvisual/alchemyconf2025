fa0ae049adb6a02ecbfaea5765421fd970db9833
josevazf <josevaz.fco@gmail.com>
2024-10-07 15:34:28 +0100

    Updated Sponsors section

--- src/app/homepage/Sponsors.tsx
+++ src/app/homepage/Sponsors.tsx
@@ -1,18 +1,28 @@
 import Button from "../_components/Button";
 import sponsors from "../_constants/sponsors.json";
+import Image from "next/image";
 
 export default function Sponsors() {
   return (
     <section
       id="sponsors"
-      className="flex flex-col items-center justify-center gap-24 overflow-hidden py-[220px]"
+      className="my-[136px] flex flex-col items-center justify-center overflow-hidden"
     >
-      <div className="font-koulen text-[128px] text-dark-blue">Sponsors</div>
-      <div className="grid w-full grid-cols-4 grid-rows-2 gap-[40px]">
+      <div className="mb-10 font-koulen text-9xl leading-[1.1] text-dark-blue">
+        Sponsors
+      </div>
+      <div className="w-[760px] text-center font-zilla_slab text-2.5xl font-medium text-dark-blue">
+        Alchemy Conf is powered by the support of our esteemed sponsors. Want to
+        join them? <br /> You get to support the Elixir community and elevate
+        your brand. A win-win.
+      </div>
+      <div className="grid w-full grid-cols-4 grid-rows-2 gap-x-11 gap-y-[74px] py-[74px]">
         {sponsors.map((sponsor) => (
-          <div key={sponsor.id} className="bg-sage p-4">
-            <img
+          <div key={sponsor.id} className="h-24 w-[288px] bg-grey p-4">
+            <Image
               src={sponsor.logo}
+              width={288}
+              height={96}
               alt={`Sponsor ${sponsor.id}`}
               className="h-auto w-full"
             />
@@ -20,7 +30,11 @@ export default function Sponsors() {
         ))}
       </div>
 
-      <Button className="bg-bordeux" text="Sponsorship Prospectus" href="/" />
+      <Button
+        className="bg-sage text-xl font-semibold text-dark-blue"
+        text="SPONSORSHIP PROSPECTUS"
+        href="/"
+      />
     </section>
   );
 }
