89813acaea44e2596cd68f49983ba4a000026604
josevazf <josevaz.fco@gmail.com>
2024-10-10 16:42:00 +0100

    Updated Move page

--- src/app/practical/move/page.tsx
+++ src/app/practical/move/page.tsx
@@ -1,62 +1,43 @@
 import PracticalCard from "@/app/_components/PracticalCard";
+import Bus1 from "@/assets/icons/bus1";
+import BusStop from "@/assets/icons/bus_stop";
 import ConnectVector from "@/assets/icons/connect_vector";
+import Bicycle from "@/assets/icons/bicycle";
+import Plane from "@/assets/icons/plane";
+import Bus2 from "@/assets/icons/bus2";
+import Train from "@/assets/icons/train";
+import moveData from "../../_constants/move.json";
 
-export default function Practical() {
+export default function Move() {
   return (
     <section className="flex flex-col items-center justify-center py-[120px]">
       <div className="pb-8 text-center font-koulen text-[128px] text-dark-blue">
         Move
       </div>
       <div className="relative flex w-[1008px] flex-col gap-10 font-zilla_slab text-dark-blue">
+        <Bicycle className="right-[170px] top-[100px]" />
         <ConnectVector variant={1} className="right-[78px] top-[150px]" />
-        <PracticalCard
-          position="left"
-          cardTitle="Walking"
-          description="Braga is a compact city, making it ideal for walking. Many key locations, including Theatro Circo, are in very walkable neighborhoods. Braga also has lovely cycling lanes if you have your bike handy."
-          linkText="City Map"
-          link="https://www.google.pt/maps/place/Braga"
-          image="/"
-          color="yellow"
-        />
+        <BusStop className="left-[84px] top-[340px]" />
+        <Bus1 className="left-[237px] top-[377px]" />
         <ConnectVector variant={2} className="left-[74px] top-[342px]" />
-        <PracticalCard
-          position="right"
-          cardTitle="By Transit"
-          description="For longer distances, Braga’s bus system can be a convenient and inexpensive choice. Here are all routes and schedules, but do keep in mind that Google Maps includes this information for the entire city of Braga."
-          linkText="Tub"
-          link="/https://tub.pt/"
-          image=""
-          color="blue"
-        />
+        <Plane className="right-[215px] top-[693px]" />
+        <Bus2 className="right-[146px] top-[782px]" />
         <ConnectVector variant={3} className="right-[78px] top-[750px]" />
-        <PracticalCard
-          position="left"
-          cardTitle="By Airport Shuttle"
-          description="Braga is served by the Francisco Sá Carneiro Airport, better known as the Porto airport. GetBus has a convenient shuttle service that will deliver you right to the city centre."
-          linkText="Getbus"
-          link="https://www.getbus.eu/en/braga-aeroporto-porto"
-          image="/"
-          color="bordeux"
-        />
+        <Train className="left-[284px] top-[1080px]" />
         <ConnectVector variant={4} className="left-[62px] top-[1060px]" />
-        <PracticalCard
-          position="right"
-          cardTitle="By Train"
-          description="Braga is well-connected by train, making it easy to get here from Porto or Lisbon. The Intercidades trains or Alfapendular service are our recommendations."
-          linkText="CP Portugal"
-          link="https://www.cp.pt/passageiros/en"
-          image="/"
-          color="sage"
-        />
-        <PracticalCard
-          position="left"
-          cardTitle="By Bus"
-          description="There are several companies operating buses from Lisbon, Porto, or anywhere else in the country, really, to Braga. Rede Expressos and FlixBus are both solid options."
-          linkText="Rede Expressos"
-          link="https://rede-expressos.pt/en"
-          image="/"
-          color="purple"
-        />
+        {moveData.map((card, key) => (
+          <div key={card.id}>
+            <PracticalCard
+              position={key % 2 === 0 ? "left" : "right"}
+              cardTitle={card.cardTitle}
+              description={card.description}
+              linkText={card.linkText}
+              link={card.link}
+              image={card.image}
+              color={card.color}
+            />
+          </div>
+        ))}
       </div>
     </section>
   );
