b42b79f0302dc988d8f4561c0fd52afff32f063a
Carolina Strecht <77847398+cstrecht@users.noreply.github.com>
2024-10-10 18:36:08 +0100

    add basic authentication to avoid leaks (#18)

---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+const USERNAME = process.env.BASIC_AUTH_USERNAME;
+const PASSWORD = process.env.BASIC_AUTH_PASSWORD;
+const basicAuth = Buffer.from(`${USERNAME}:${PASSWORD}`).toString("base64");
+
+export const config = {
+  matcher: ["/"],
+};
+
+export function middleware(request: NextRequest) {
+  console.log("Middleware is running!");
+  const authorizationHeader = request.headers.get("authorization");
+
+  if (authorizationHeader) {
+    const [type, credentials] = authorizationHeader.split(" ");
+
+    if (type === "Basic" && credentials === basicAuth) {
+      return NextResponse.next();
+    }
+  }
+
+  return new NextResponse("Unauthorized", {
+    status: 401,
+    headers: {
+      "WWW-Authenticate": 'Basic realm="Secure Area"',
+    },
+  });
+}
