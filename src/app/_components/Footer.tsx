0a22ef4184d6484fb16c774c96dd8dd0e0b88d4e
josevazf <josevaz.fco@gmail.com>
2024-10-22 22:44:54 +0100

    Updated Footer

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -1,45 +1,52 @@
 import Link from "next/link";
 import AlchemySquareIcon from "@/assets/icons/alchemy_square_icon";
+import FooterBlob from "@/assets/icons/footer_blob";
 
 export default function Footer() {
   return (
-    <footer className="z-50 flex w-full justify-between bg-bordeux p-20 pr-32 font-zilla_slab text-background">
-      <Link href="/" className="flex h-fit gap-4 outline-none">
-        <AlchemySquareIcon className="h-12 w-12" />
-        <p className="font-koulen text-[25px]">Alchemy Conf</p>
-      </Link>
-      <div className="flex flex-col gap-2 text-[18px]">
-        <p className="pb-6 text-[20px] font-semibold">About</p>
-        <Link href="/about">About Organizers</Link>
-        <Link href="/about">About Alchemy Conf</Link>
-        <Link href="/about">Code of Conduct</Link>
+    <div className="relative w-full">
+      {" "}
+      <div className="absolute bottom-0 left-1/2 -z-10 -translate-x-1/2 transform">
+        <FooterBlob className="w-full max-w-[1440px]" />{" "}
       </div>
-      <div className="flex flex-col gap-2 text-[18px]">
-        <p className="pb-6 text-[20px] font-semibold">Practical</p>
-        <Link href="/under-construction">Schedule</Link>
-        <Link href="/practical/move">Move</Link>
-        <Link href="/under-construction">Eat</Link>
-        <Link href="/under-construction">Party & Mingle</Link>
-        <Link href="/practical/sleep">Sleep</Link>
-      </div>
-      <div className="flex flex-col gap-2 text-[18px]">
-        <p className="pb-6 text-[20px] font-semibold">Alchemy Conf 2025</p>
-        <Link href="/under-construction">Talks & Speakers</Link>
-        <Link href="/under-construction">Workshops</Link>
-      </div>
-      <div className="flex flex-col gap-2 text-[18px]">
-        <p className="pb-6 text-[20px] font-semibold">Sponsors</p>
-        <Link href="/under-construction">
-          Sponsorship
-          <br /> Prospectus
+      <footer className="z-50 mx-auto flex w-[1440px] justify-between bg-none p-20 pr-32 font-zilla_slab text-background">
+        <Link href="/" className="flex h-fit gap-4 outline-none">
+          <AlchemySquareIcon className="h-12 w-12" />
+          <p className="font-koulen text-[25px]">Alchemy Conf</p>
         </Link>
-      </div>
-      <div className="flex flex-col gap-2 text-[18px]">
-        <p className="pb-6 text-[20px] font-semibold">Follow us</p>
-        <Link href="https://x.com/alchemy_conf" target="_blank">
-          X
-        </Link>
-      </div>
-    </footer>
+        <div className="flex flex-col gap-2 text-[18px]">
+          <p className="pb-6 text-[20px] font-semibold">About</p>
+          <Link href="/about">About Organizers</Link>
+          <Link href="/about">About Alchemy Conf</Link>
+          <Link href="/about">Code of Conduct</Link>
+        </div>
+        <div className="flex flex-col gap-2 text-[18px]">
+          <p className="pb-6 text-[20px] font-semibold">Practical</p>
+          <Link href="/under-construction">Schedule</Link>
+          <Link href="/practical/move">Move</Link>
+          <Link href="/under-construction">Eat</Link>
+          <Link href="/under-construction">Party & Mingle</Link>
+          <Link href="/practical/sleep">Sleep</Link>
+        </div>
+        <div className="flex flex-col gap-2 text-[18px]">
+          <p className="pb-6 text-[20px] font-semibold">Alchemy Conf 2025</p>
+          <Link href="/under-construction">Talks & Speakers</Link>
+          <Link href="/under-construction">Workshops</Link>
+        </div>
+        <div className="flex flex-col gap-2 text-[18px]">
+          <p className="pb-6 text-[20px] font-semibold">Sponsors</p>
+          <Link href="/under-construction">
+            Sponsorship
+            <br /> Prospectus
+          </Link>
+        </div>
+        <div className="flex flex-col gap-2 text-[18px]">
+          <p className="pb-6 text-[20px] font-semibold">Follow us</p>
+          <Link href="https://x.com/alchemy_conf" target="_blank">
+            X
+          </Link>
+        </div>
+      </footer>
+    </div>
   );
 }
---
+++ src/assets/icons/footer_blob.tsx
@@ -0,0 +1,30 @@
+import { SVGProps } from "react";
+
+interface BlobProps extends SVGProps<SVGSVGElement> {
+  className?: string;
+  color?: string;
+}
+
+const FooterBlob = ({ className, color = "#7D1D3F", ...props }: BlobProps) => {
+  return (
+    <div className={`relative ${className}`}>
+      <svg
+        width="2740"
+        height="545"
+        viewBox="0 0 2740 545"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        {...props}
+      >
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M-11.9567 217.816C-86.7926 356.843 -79.4987 479.403 -11.9567 622.116C82.7774 822.284 427.428 618.408 645.648 656.113C1012.35 719.473 1293.3 873.671 1466.15 544.115C1559.81 365.537 1548 191 1433 79C1224.21 -124.341 994.522 127.294 705.535 165.069C426.951 201.484 121.209 -29.5749 -11.9567 217.816Z"
+          fill={color}
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default FooterBlob;
