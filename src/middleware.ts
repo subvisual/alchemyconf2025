5f458c454df6557cfc758e71b293a3f5e4707e67
Carolina Strecht <carolina.stsampaio@gmail.com>
2024-10-31 11:53:23 +0000

    only run middleware in staging

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,7 @@ import type { NextRequest } from "next/server";
 
 const USERNAME = process.env.BASIC_AUTH_USERNAME;
 const PASSWORD = process.env.BASIC_AUTH_PASSWORD;
+const STAGING_URL = process.env.STAGING_URL;
 const basicAuth = Buffer.from(`${USERNAME}:${PASSWORD}`).toString("base64");
 
 export const config = {
@@ -10,6 +11,10 @@ export const config = {
 };
 
 export function middleware(request: NextRequest) {
+  if (request.nextUrl.origin != STAGING_URL) {
+    return NextResponse.next();
+  }
+
   const authorizationHeader = request.headers.get("authorization");
 
   if (authorizationHeader) {
