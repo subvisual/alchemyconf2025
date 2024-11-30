ae4de74a555ecc516243ebb1bff85fa368600033
josevazf <josevaz.fco@gmail.com>
2024-11-30 12:50:15 +0000

    Update header sections

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
       <div className="absolute bottom-0 left-[calc(50%-70px)] -z-10 -translate-x-1/2 transform">
         <FooterBlob className="w-full max-w-[1440px] translate-y-[0px] overflow-visible tablet:translate-y-[80px]" />
       </div>
-      <footer className="z-50 mx-auto flex max-w-[1440px] flex-col justify-around bg-none p-10 pb-8 font-alegreya_sans text-lg font-light text-background tablet:flex-row tablet:p-20 tablet:pb-16 tablet:text-xl">
+      <footer className="z-50 mx-auto flex max-w-desktop flex-col justify-around bg-none py-10 pb-8 font-alegreya_sans text-lg font-light text-background tablet:flex-row tablet:py-20 tablet:pb-16 tablet:text-xl">
         <Link
           href="/"
           className="mb-4 flex h-fit translate-y-[-10px] items-center gap-4 outline-none tablet:mb-0"
