a6f4ceb12828c08083a0d33241be30a105a7ef52
josevazf <josevaz.fco@gmail.com>
2024-12-03 18:28:25 +0000

    Refactor + fixes

--- src/app/homepage/Speakers.tsx
+++ src/app/homepage/Speakers.tsx
@@ -1,47 +1,24 @@
 "use client";
 import { useEffect, useState } from "react";
-import AltButton from "../_components/AltButton";
 import SpeakerBlob from "@/assets/icons/speaker_blob";
 import SpeakerTitleBlob from "@/assets/icons/speaker_title_blob";
 import speakers from "../_constants/speakers.json";
-import speaker1 from "@/assets/images/pic_aaron_cruz.png";
-import speaker2 from "@/assets/images/pic_andre_albuquerque.png";
-import speaker3 from "@/assets/images/pic_andrea_leopardi.jpg";
-import speaker4 from "@/assets/images/pic_bruce_tate.png";
-import speaker5 from "@/assets/images/pic_hugo_barauna.jpg";
-import speaker6 from "@/assets/images/pic_julia_mathias.jpg";
-import speaker7 from "@/assets/images/pic_sasa_juric.png";
-import speaker8 from "@/assets/images/pic_shannon_ryan.jpg";
-import speaker9 from "@/assets/images/pic_tobias_pfeiffer.jpg";
-import speaker10 from "@/assets/images/pic_wojtek_mach.jpg";
-import speaker11 from "@/assets/images/pic_zach_daniel.jpg";
+import speakerImages from "../_constants/speakers_images";
 import SpeakerBlobMobile from "@/assets/icons/speaker_blob_mobile";
 import SpeakerTitleBlobMobile from "@/assets/icons/speaker_title_blob_mobile";
 
 export default function Speakers() {
-  // const isNonChromium =
-  //   typeof navigator !== "undefined" && /firefox/i.test(navigator.userAgent);
   const [isNonChromium, setIsNonChromium] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
     const isNonChromiumBrowser =
       /firefox|safari/i.test(navigator.userAgent) &&
       !/chrome|android/i.test(navigator.userAgent);
     setIsNonChromium(isNonChromiumBrowser);
+    setIsMounted(true);
   }, []);
-  const speakerImages = [
-    speaker1,
-    speaker2,
-    speaker3,
-    speaker4,
-    speaker5,
-    speaker6,
-    speaker7,
-    speaker8,
-    speaker9,
-    speaker10,
-    speaker11,
-  ];
+
   return (
     <section
       id="speakers"
@@ -60,77 +37,68 @@ export default function Speakers() {
           they’ve learned about building, innovation, and real-world
           applications.
         </div>
-        {speakers.map((speakers) => (
-          <a
-            href={`/speakers#speaker-${speakers.id}`}
-            key={speakers.id}
-            className="speaker-transform relative mx-auto flex h-[350px] w-[400px] items-center justify-center xs:ml-[-20px] tablet:h-[500px] tablet:w-[600px] desktop:w-[600px]"
-            style={{
-              // @ts-ignore
-              "--translate-mobile": `translateX(${isNonChromium ? speakers.m_blob_x_ff : speakers.m_blob_x}px) translateY(${speakers.m_blob_y}px)`,
-              "--translate-tablet": `translateX(${speakers.t_blob_x}px) translateY(${speakers.t_blob_y}px)`,
-              "--translate-desktop": `translateX(${isNonChromium ? speakers.blob_x_ff : speakers.blob_x}px) translateY(${speakers.blob_y}px)`,
-            }}
-          >
-            {/* desktop / tablet */}
-            <div className="hidden tablet:flex">
-              <SpeakerBlob
-                src={speakerImages[speakers.id - 1].src}
-                alt={speakers.name}
-                height="550px"
-                width="550px"
-                position={String(speakers.id)}
-                blobType={speakers.blobType}
-                imageX={speakers.image_x}
-                imageY={speakers.image_y}
-                imageScale={Number(speakers.image_scale)}
-              />
-              <SpeakerTitleBlob
-                name={speakers.name}
-                surname={speakers.surname}
-                variant={Number(speakers.titleBlobType)}
-                xLink={speakers.x_link}
-                gitLink={speakers.git_link}
-                webLink={speakers.web_link}
-              />
-            </div>
+        {isMounted && (
+          speakers.map((speakers) => (
+            <a
+              href={`/speakers#speaker-${speakers.id}`}
+              key={speakers.id}
+              className="speaker-transform relative mx-auto flex h-[350px] w-[400px] items-center justify-center xs:ml-[-20px] tablet:h-[500px] tablet:w-[600px] desktop:w-[600px]"
+              style={{
+                // @ts-ignore
+                "--translate-mobile": `translateX(${isNonChromium ? speakers.m_blob_x_ff : speakers.m_blob_x}px) translateY(${speakers.m_blob_y}px)`,
+                "--translate-tablet": `translateX(${speakers.t_blob_x}px) translateY(${speakers.t_blob_y}px)`,
+                "--translate-desktop": `translateX(${isNonChromium ? speakers.blob_x_ff : speakers.blob_x}px) translateY(${speakers.blob_y}px)`,
+              }}
+            >
+              {/* desktop / tablet */}
+              <div className="hidden tablet:flex">
+                <SpeakerBlob
+                  src={speakerImages[speakers.id - 1].src}
+                  alt={speakers.name}
+                  height="550px"
+                  width="550px"
+                  position={String(speakers.id)}
+                  blobType={speakers.blobType}
+                  imageX={speakers.image_x}
+                  imageY={speakers.image_y}
+                  imageScale={Number(speakers.image_scale)}
+                />
+                <SpeakerTitleBlob
+                  name={speakers.name}
+                  surname={speakers.surname}
+                  variant={Number(speakers.titleBlobType)}
+                  xLink={speakers.x_link}
+                  gitLink={speakers.git_link}
+                  webLink={speakers.web_link}
+                />
+              </div>
 
-            {/* mobile */}
-            <div className="flex tablet:hidden">
-              <SpeakerBlobMobile
-                src={speakerImages[speakers.id - 1].src}
-                alt={speakers.name}
-                height="550px"
-                width="550px"
-                position={String(speakers.id)}
-                blobType={speakers.blobType}
-                imageX={speakers.image_x_mob}
-                imageY={speakers.image_y_mob}
-                imageScale={Number(speakers.image_scale_mob)}
-              />
-              <SpeakerTitleBlobMobile
-                name={speakers.name}
-                surname={speakers.surname}
-                variant={Number(speakers.titleBlobType)}
-                xLink={speakers.x_link}
-                gitLink={speakers.git_link}
-                webLink={speakers.web_link}
-              />
-            </div>
-          </a>
-        ))}
-        {/* <div className="mt-[70px] flex justify-center font-alegreya_sans text-xl font-medium text-dark-blue tablet:mt-[-90px] tablet:text-[28px] tablet:leading-[36px] desktop:ml-[150px] desktop:mt-[200px] desktop:text-[32px] desktop:leading-[44px]"> */}
-        {/*   {" "} */}
-        {/*   ...and many more */}
-        {/* </div> */}
+              {/* mobile */}
+              <div className="flex tablet:hidden">
+                <SpeakerBlobMobile
+                  src={speakerImages[speakers.id - 1].src}
+                  alt={speakers.name}
+                  height="550px"
+                  width="550px"
+                  position={String(speakers.id)}
+                  blobType={speakers.blobType}
+                  imageX={speakers.image_x_mob}
+                  imageY={speakers.image_y_mob}
+                  imageScale={Number(speakers.image_scale_mob)}
+                />
+                <SpeakerTitleBlobMobile
+                  name={speakers.name}
+                  surname={speakers.surname}
+                  variant={Number(speakers.titleBlobType)}
+                  xLink={speakers.x_link}
+                  gitLink={speakers.git_link}
+                  webLink={speakers.web_link}
+                />
+              </div>
+            </a>
+          ))
+        )}
       </div>
-      {/* <div className="relative mb-[336px] mt-[-250px] flex w-full flex-col items-end pr-[150px]"> */}
-      {/*   <div className="pb-[20px] pr-[100px] font-alegreya_sans text-2.5xl font-medium text-dark-blue"> */}
-      {/*     {" "} */}
-      {/*     ...and many more */}
-      {/*   </div> */}
-      {/*   <AltButton href="" className="font-medium" text="See all Speakers" /> */}
-      {/* </div> */}
     </section>
   );
 }
--- src/app/speakers/SpeakersGeneral.tsx
+++ src/app/speakers/SpeakersGeneral.tsx
@@ -4,31 +4,7 @@ import GithubIcon from "@/assets/icons/icon_github";
 import WebIcon from "@/assets/icons/icon_web";
 
 import speakers from "../_constants/speakers.json";
-import speaker1 from "@/assets/images/pic_aaron_cruz.png";
-import speaker2 from "@/assets/images/pic_andre_albuquerque.png";
-import speaker3 from "@/assets/images/pic_andrea_leopardi.jpg";
-import speaker4 from "@/assets/images/pic_bruce_tate.png";
-import speaker5 from "@/assets/images/pic_hugo_barauna.jpg";
-import speaker6 from "@/assets/images/pic_julia_mathias.jpg";
-import speaker7 from "@/assets/images/pic_sasa_juric.png";
-import speaker8 from "@/assets/images/pic_shannon_ryan.jpg";
-import speaker9 from "@/assets/images/pic_tobias_pfeiffer.jpg";
-import speaker10 from "@/assets/images/pic_wojtek_mach.jpg";
-import speaker11 from "@/assets/images/pic_zach_daniel.jpg";
-
-const speakerImages = [
-  speaker1,
-  speaker2,
-  speaker3,
-  speaker4,
-  speaker5,
-  speaker6,
-  speaker7,
-  speaker8,
-  speaker9,
-  speaker10,
-  speaker11,
-];
+import speakerImages from "../_constants/speakers_images";
 
 interface InfoSectionProps {
   title: string;
@@ -43,21 +19,18 @@ const InfoSection = ({
 }: InfoSectionProps) => (
   <div className="flex gap-8 pl-8 pb-8">
     <div className="flex items-stretch">
-      <div className="relative flex w-8 flex-col items-start justify-start">
+      <div className="relative flex w-10 flex-col items-start justify-start">
         <p
           className={`justify font-normal uppercase text-${color}`}
           style={{
             writingMode: "vertical-rl",
-            transform: "rotate(180deg) translateY(-6%)",
+            transform: "rotate(180deg)",
           }}
         >
           {title}
         </p>
         <span
           className={`absolute right-0 top-0 h-full w-1 bg-${color}`}
-          style={{
-            top: "4%",
-          }}
         ></span>
       </div>
     </div>
@@ -67,14 +40,39 @@ const InfoSection = ({
   </div>
 );
 
+interface SpeakerDetailsProps {
+  name: string;
+  surname: string;
+  x_link: string;
+  git_link: string;
+  web_link: string;
+}
+
+const SpeakerDetails = ({ name, surname, x_link, git_link, web_link }: SpeakerDetailsProps) => (
+  <>
+    <h1 className="relative text-left font-koulen text-[50px] leading-[50px] tablet:text-[80px] tablet:leading-[80px] desktop:text-[100px] desktop:leading-[100px]">
+      {name + " " + surname}{" "}
+      <span className="absolute left-0 top-0 -z-10 translate-x-1 translate-y-1 text-[#7D1D3F3D]">
+        {name + " " + surname}{" "}
+      </span>
+    </h1>
+    <div className="mt-[-20px] flex items-center justify-start space-x-4 pl-16 pb-8">
+      <p className="font-bold">FOLLOW</p>
+      <XIcon link={x_link} />
+      <GithubIcon link={git_link} />
+      <WebIcon link={web_link} />
+    </div>
+  </>
+);
+
 export default function SpeakersGeneral() {
   return (
-    <section className="mb-20 mt-40 tablet:mb-28 tablet:mt-56 desktop:mb-40 desktop:mt-64">
+    <section className="mb-20 mt-40 tablet:mb-28 tablet:mt-56 desktop:mb-40 desktop:mt-64 flex flex-col items-center justify-center">
       {speakers.map((speaker) => (
         <div
           key={speaker.id}
           id={`speaker-${speaker.id}`}
-          className="mb-20 flex w-full max-w-screen-desktop scroll-mt-48 flex-row gap-6 p-6"
+          className="mb-20 flex w-full max-w-screen-desktop scroll-mt-48 flex-row gap-6 p-6 justify-center"
         >
           {speaker.id % 2 === 1 ? (
             <>
@@ -91,19 +89,14 @@ export default function SpeakersGeneral() {
                   imageScale={Number(speaker.image_scale)}
                 />
               </div>
-              <div className="md:w-2/3 flex flex-col gap-4 font-alegreya_sans text-xl font-normal text-dark-blue">
-                <h1 className="relative text-left font-koulen text-[50px] leading-[50px] text-dark-blue tablet:text-[80px] tablet:leading-[80px] desktop:text-[100px] desktop:leading-[100px]">
-                  {speaker.name + " " + speaker.surname}{" "}
-                  <span className="absolute left-0 top-0 -z-10 translate-x-1 translate-y-1 text-[#7D1D3F3D]">
-                    {speaker.name + " " + speaker.surname}{" "}
-                  </span>
-                </h1>
-                <div className="mt-[-20px] flex items-center justify-center space-x-4 pb-8">
-                  <p className="font-bold text-dark-blue">FOLLOW</p>
-                  <XIcon link={speaker.x_link} />
-                  <GithubIcon link={speaker.git_link} />
-                  <WebIcon link={speaker.web_link} />
-                </div>
+              <div className="md:w-2/3 flex flex-col gap-4 font-alegreya_sans text-[24px] font-normal text-dark-blue">
+                <SpeakerDetails 
+                  name={speaker.name} 
+                  surname={speaker.surname} 
+                  x_link={speaker.x_link} 
+                  git_link={speaker.git_link} 
+                  web_link={speaker.web_link} 
+                />
                 <InfoSection
                   title="Short Bio"
                   content={speaker.bio || ""}
@@ -127,19 +120,14 @@ export default function SpeakersGeneral() {
             </>
           ) : (
             <>
-              <div className="md:w-2/3 flex flex-col gap-4 font-alegreya_sans text-xl font-normal text-dark-blue">
-                <h1 className="relative text-left font-koulen text-[50px] leading-[50px] text-dark-blue tablet:text-[80px] tablet:leading-[80px] desktop:text-[100px] desktop:leading-[100px]">
-                  {speaker.name + " " + speaker.surname}{" "}
-                  <span className="absolute left-0 top-0 -z-10 translate-x-1 translate-y-1 text-[#7D1D3F3D]">
-                    {speaker.name + " " + speaker.surname}{" "}
-                  </span>
-                </h1>
-                <div className="mt-[-20px] flex items-center justify-center space-x-4 pb-8">
-                  <p className="font-bold text-dark-blue">FOLLOW</p>
-                  <XIcon link={speaker.x_link} />
-                  <GithubIcon link={speaker.git_link} />
-                  <WebIcon link={speaker.web_link} />
-                </div>
+              <div className="md:w-2/3 flex flex-col gap-4 font-alegreya_sans text-[24px] font-normal text-dark-blue">
+                <SpeakerDetails 
+                  name={speaker.name} 
+                  surname={speaker.surname} 
+                  x_link={speaker.x_link} 
+                  git_link={speaker.git_link} 
+                  web_link={speaker.web_link} 
+                />
                 <InfoSection
                   title="Short Bio"
                   content={speaker.bio || ""}
