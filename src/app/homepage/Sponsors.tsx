d7b205517a0c4ab2bb169d1c003054fe5aa43efd
José Vaz Francisco <josevaz.fco@gmail.com>
2025-02-26 10:14:45 +0000

    Add Maersk (#57)

--- src/app/homepage/Sponsors.tsx
+++ src/app/homepage/Sponsors.tsx
@@ -4,12 +4,13 @@
 import sponsors from "../_constants/sponsors.json";
 import AltButton from "../_components/AltButton";
 import remoteLogo from "@/assets/images/remote_logo.png";
-import SponsorGoldBlob from "@/assets/icons/sponsor_gold_blob";
-import MainSponsorBlob from "@/assets/icons/main_sponsor_blob";
 import SponsorMainBlob from "@/assets/icons/sponsor_main_blob";
+import SponsorGoldBlob from "@/assets/icons/sponsor_gold_blob";
+import SponsorStoneBlob from "@/assets/icons/sponsor_stone_blob";
 import sponsorLogos from "../_constants/sponsors_logos";
 import SponsorBlobs from "@/assets/icons/sponsor_blobs";
 import useScreenSize, { ScreenSize } from "@/hooks/useScreenSize";
+
 
 type SponsorLogoKey = keyof typeof sponsorLogos;
 
@@ -69,7 +70,7 @@ export default function Sponsors() {
             Main Sponsor
           </div>
           <a href={sponsors.main[0].link} target="_blank">
-            <MainSponsorBlob
+            <SponsorMainBlob
               imageSrc={remoteLogo.src}
               className="left-[40px] top-[-50px] mobile:translate-x-[-40px] mobile:scale-[0.6] tablet:left-[45px] tablet:top-[0px] tablet:scale-[0.8] desktop:top-[30px] desktop:scale-[1.0]"
             />
@@ -80,7 +81,7 @@ export default function Sponsors() {
             Alchemy Conf is powered by the support of our esteemed sponsors.
             <br />
             <br />
-            <span className="font-bold ">Want to join them?</span>
+            <span className="font-bold">Want to join them?</span>
             You get to support the Elixir community and elevate your brand. A
             win-win.
             <AltButton
@@ -93,15 +94,15 @@ export default function Sponsors() {
       </div>
 
       {/* GOLD SPONSORS */}
-      <div className="relative mobile:mb-[-270px] tablet:mb-16 desktop:mb-32">
+      <div className="relative mobile:mb-[-300px] tablet:mb-16 desktop:mb-32">
         <div className="text-center font-alegreya_sans font-bold text-dark-blue mobile:text-[24px] tablet:absolute tablet:left-[170px] tablet:top-[120px] tablet:text-[28px] desktop:left-[100px] desktop:top-[80px] desktop:text-[32px]">
           Gold Sponsors
         </div>
-        <SponsorMainBlob className="mobile:scale-[0.4] top-[-180px] tablet:top-0 tablet:scale-[0.9] desktop:scale-[1.1]" />
-        <div className="absolute left-1/2 top-[80px] tablet:top-[215px] z-10 -translate-x-1/2">
-          <div className="grid gap-x-[0px] w-[340px] mobile:grid-cols-2 tablet:gap-x-20 tablet:grid-cols-3 tablet:ml-[-60px] tablet:w-[800px] desktop:ml-0 desktop:gap-x-20 desktop:w-[1000px]">
+        <SponsorGoldBlob className="top-[-180px] mobile:scale-[0.4] tablet:top-0 tablet:scale-[0.9] desktop:scale-[1.1]" />
+        <div className="absolute left-1/2 top-[80px] z-10 -translate-x-1/2 tablet:top-[215px]">
+          <div className="grid w-[340px] gap-x-[0px] mobile:grid-cols-2 tablet:ml-[-60px] tablet:w-[800px] tablet:grid-cols-3 tablet:gap-x-20 desktop:ml-0 desktop:w-[1000px] desktop:gap-x-20">
             {sponsors.gold.map((sponsor) => (
-              <a href={sponsor.link} target="_blank" key={sponsor.id}>
+              <a href={sponsor.link} target="_blank" rel="noopener noreferrer" key={sponsor.id}>
                 <SponsorBlobs
                   imageSrc={sponsorLogos[sponsor.logo as SponsorLogoKey].src}
                   id={sponsor.id.toString()}
@@ -115,27 +116,53 @@ export default function Sponsors() {
         </div>
       </div>
 
+      {/* STONE SPONSORS */}
+      <div className="relative mobile:mb-[-550px] tablet:mb-16 desktop:mb-32">
+        <div className="text-center font-alegreya_sans font-bold text-background mobile:translate-y-[70px] mobile:text-[24px] tablet:absolute tablet:left-[260px] tablet:top-[140px] tablet:text-[28px] desktop:left-[180px] desktop:top-[70px] desktop:text-[32px]">
+          Stone Sponsors
+        </div>
+        <SponsorStoneBlob className="top-[-350px] mobile:scale-[0.4] tablet:top-0 tablet:scale-[0.9] desktop:scale-[1.1]" />
+        <div className="absolute left-1/2 top-[100px] z-10 -translate-x-1/2 tablet:top-[300px] desktop:top-[275px]">
+          <div className="grid w-[340px] gap-x-[0px] mobile:grid-cols-2 tablet:ml-[-60px] tablet:w-[800px] tablet:grid-cols-3 tablet:gap-x-20 desktop:ml-0 desktop:w-[1000px] desktop:gap-x-20">
+            {sponsors.stone.map((sponsor) => (
+              <a href={sponsor.link} target="_blank" rel="noopener noreferrer" key={sponsor.id}>
+                <SponsorBlobs
+                  imageSrc={sponsorLogos[sponsor.logo as SponsorLogoKey].src}
+                  id={sponsor.id.toString()}
+                  className="scale-[0.5] tablet:scale-[0.8] desktop:scale-[1.0]"
+                  blobVariant={sponsor.id - 1}
+                  scale={Number(sponsor.scale)}
+                  typeStart={1}
+                  x={sponsor.x}
+                  y={sponsor.y}
+                />
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
+
       {/* COFFEE BREAK */}
       <div className="relative">
-      {/* <div className="relative hidden desktop:flex"> */}
+        {/* <div className="relative hidden desktop:flex"> */}
         {/* <SponsorGoldBlob color="bg-background" className="z-0" /> */}
-        <div className="text-center top-[60px] font-alegreya_sans font-bold text-dark-blue tablet:text-[28px] mobile:text-[24px] tablet:absolute tablet:left-[20px] desktop:top-[0px] desktop:left-[20px] desktop:text-[32px]">
+        <div className="top-[60px] text-center font-alegreya_sans font-bold text-dark-blue mobile:text-[24px] tablet:absolute tablet:left-[20px] tablet:text-[28px] desktop:left-[20px] desktop:top-[0px] desktop:text-[32px]">
           Coffee Break Sponsors
         </div>
-        <div className="ml-[-50px] space-y-[-80px] tablet:left-[180px] top-[100px] tablet:py-[124px] tablet:space-y-[60px] desktop:space-y-[86px]">
+        <div className="top-[100px] ml-[-50px] space-y-[-80px] tablet:left-[180px] tablet:space-y-[60px] tablet:py-[124px] desktop:space-y-[86px]">
           {coffeeBreakRows.map((row, rowIndex) => (
             <div
               key={rowIndex}
-              className={`grid gap-x-0 tablet:gap-x-20 grid-cols-2 tablet:grid-cols-3 ${rowIndex % 2 !== 0 ? "tablet:translate-x-[120px]" : ""} transition-transform`}
+              className={`grid grid-cols-2 gap-x-0 tablet:grid-cols-3 tablet:gap-x-20 ${rowIndex % 2 !== 0 ? "tablet:translate-x-[120px]" : ""} transition-transform`}
             >
               {row.map((sponsor) => (
-                <a href={sponsor.link} target="_blank" key={sponsor.id}>
+                <a href={sponsor.link} target="_blank" rel="noopener noreferrer" key={sponsor.id}>
                   <SponsorBlobs
-                    imageSrc={sponsorLogos[sponsor.logo].src}
+                    imageSrc={sponsorLogos[sponsor.logo as SponsorLogoKey].src}
                     id={sponsor.id.toString()}
                     className="scale-[0.5] tablet:scale-[0.8] desktop:scale-[1.0]"
                     blobVariant={sponsor.id - 1}
-                    scale={sponsor.scale}
+                    scale={Number(sponsor.scale)}
                     typeStart={1}
                     x={sponsor.x}
                     y={sponsor.y}
@@ -149,25 +176,25 @@ export default function Sponsors() {
 
       {/* PARTNERS */}
       <div className="relative">
-      {/* <div className="relative hidden desktop:flex"> */}
+        {/* <div className="relative hidden desktop:flex"> */}
         {/* <SponsorGoldBlob color="bg-background" className="z-0" /> */}
-        <div className="text-center top-[60px] font-alegreya_sans font-bold text-dark-blue tablet:text-[28px] mobile:text-[24px] tablet:absolute tablet:left-[20px] desktop:top-[0px] desktop:left-[20px] desktop:text-[32px]">
+        <div className="top-[60px] text-center font-alegreya_sans font-bold text-dark-blue mobile:text-[24px] tablet:absolute tablet:left-[20px] tablet:text-[28px] desktop:left-[20px] desktop:top-[0px] desktop:text-[32px]">
           Our Partners
         </div>
-        <div className="ml-[-50px] space-y-[-80px] tablet:left-[180px] top-[100px] tablet:py-[124px] tablet:space-y-[60px] desktop:space-y-[86px]">
+        <div className="top-[100px] ml-[-50px] space-y-[-80px] tablet:left-[180px] tablet:space-y-[60px] tablet:py-[124px] desktop:space-y-[86px]">
           {partnerRows.map((row, rowIndex) => (
             <div
               key={rowIndex}
-              className={`grid gap-x-0 tablet:gap-x-20 grid-cols-2 tablet:grid-cols-3 ${rowIndex % 2 !== 0 ? "tablet:translate-x-[120px]" : ""} transition-transform`}
+              className={`grid grid-cols-2 gap-x-0 tablet:grid-cols-3 tablet:gap-x-20 ${rowIndex % 2 !== 0 ? "tablet:translate-x-[120px]" : ""} transition-transform`}
             >
               {row.map((sponsor) => (
-                <a href={sponsor.link} target="_blank" key={sponsor.id}>
+                <a href={sponsor.link} target="_blank" rel="noopener noreferrer" key={sponsor.id}>
                   <SponsorBlobs
-                    imageSrc={sponsorLogos[sponsor.logo].src}
+                    imageSrc={sponsorLogos[sponsor.logo as SponsorLogoKey].src}
                     id={sponsor.id.toString()}
                     className="scale-[0.5] tablet:scale-[0.8] desktop:scale-[1.0]"
                     blobVariant={sponsor.id - 1}
-                    scale={sponsor.scale}
+                    scale={Number(sponsor.scale)}
                     typeStart={0}
                     x={sponsor.x}
                     y={sponsor.y}
