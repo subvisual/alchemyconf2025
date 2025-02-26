f0f625d7697a4886a0e8e90800c704d5a9447ccc
josevazf <josevaz.fco@gmail.com>
2025-02-26 09:57:44 +0000

    Add stone sponsors + fixes

--- src/assets/icons/sponsor_main_blob.tsx
+++ src/assets/icons/sponsor_main_blob.tsx
@@ -3,29 +3,46 @@ import { SVGProps } from "react";
 interface BlobProps extends SVGProps<SVGSVGElement> {
   className?: string;
   color?: string;
+  imageSrc: string;
 }
 
 const SponsorMainBlob = ({
   className,
   color = "#F4AC45",
+  imageSrc,
   ...props
 }: BlobProps) => {
   return (
-    <div className={`relative -z-10 ${className}`}>
+    <div className={`relative ${className}`}>
       <svg
-        width="1119"
-        height="668"
-        viewBox="0 0 1119 668"
+        width="390"
+        height="340"
+        viewBox="0 0 290 240"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
+        style={{ transform: 'rotate(-4deg)' }}
         {...props}
       >
-        <path
-          fillRule="evenodd"
-          clipRule="evenodd"
-          d="M879.49 113.601C719.503 88.8007 602.113 166.29 502.603 134.547C354.507 87.3049 334.961 -7.47936 223.127 0.652443C111.293 8.78418 -2.98133 101.136 0.14416 256.614C2.83289 390.366 69.728 489.605 182.253 516.318C299.102 544.056 403.406 472.533 506.139 563.23C608.229 653.359 736.693 679.54 855.514 662.929C974.335 646.318 1107.85 566.858 1118.19 396.888C1129.24 215.238 951.147 124.709 879.49 113.601Z"
-          fill={color}
-        />
+        <defs>
+          <clipPath id="blobClip">
+            <path
+              d="M8 0C3.58172 0 0 3.58171 0 7.99999V182.887C13.9738 175.714 30.3824 169.001 50.9838 164.11C77.0679 157.917 103.608 168.275 133.61 179.984C166.476 192.811 203.497 207.259 248.624 203.349C258.973 202.452 273.775 197.727 290 189.503V8C290 3.58172 286.418 0 282 0H8Z"
+            />
+          </clipPath>
+        </defs>
+        <g clipPath="url(#blobClip)">
+          <path
+            d="M8 0C3.58172 0 0 3.58171 0 7.99999V182.887C13.9738 175.714 30.3824 169.001 50.9838 164.11C77.0679 157.917 103.608 168.275 133.61 179.984C166.476 192.811 203.497 207.259 248.624 203.349C258.973 202.452 273.775 197.727 290 189.503V8C290 3.58172 286.418 0 282 0H8Z"
+            fill="white"
+          />
+          <image
+            href={imageSrc}
+            width="70%"
+            height="100%"
+            // preserveAspectRatio="xMidYMid meet"
+            transform="translate(50, -25) rotate(4, 143, 100)"
+          />
+        </g>
       </svg>
     </div>
   );
@@ -33,6 +50,3 @@ const SponsorMainBlob = ({
 
 export default SponsorMainBlob;
 
-<svg width="1119" height="668" viewBox="0 0 1119 668" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path fillRule="evenodd" clipRule="evenodd" d="M879.49 113.601C719.503 88.8007 602.113 166.29 502.603 134.547C354.507 87.3049 334.961 -7.47936 223.127 0.652443C111.293 8.78418 -2.98133 101.136 0.14416 256.614C2.83289 390.366 69.728 489.605 182.253 516.318C299.102 544.056 403.406 472.533 506.139 563.23C608.229 653.359 736.693 679.54 855.514 662.929C974.335 646.318 1107.85 566.858 1118.19 396.888C1129.24 215.238 951.147 124.709 879.49 113.601Z" fill="#F4AC45"/>
-</svg>
---
+++ src/assets/icons/sponsor_stone_blob.tsx
@@ -0,0 +1,34 @@
+import { SVGProps } from "react";
+
+interface BlobProps extends SVGProps<SVGSVGElement> {
+  className?: string;
+  color?: string;
+}
+
+const SponsorStoneBlob = ({
+  className,
+  color = "#003049",
+  ...props
+}: BlobProps) => {
+  return (
+    <div className={`relative ${className} -z-10`}>
+      <svg
+        width="1289"
+        height="1047"
+        viewBox="0 0 1289 1047"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        {...props}
+      >
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M2.49991 795.019C-18.3472 683.341 109 586.015 143 505.511C177 425.008 29.2072 292.012 120 152.012C251 -49.9867 505.775 68.6634 650.5 113.011C814.716 163.332 946.247 -43.9615 1192.5 9.51112C1326 38.5 1327.88 359.886 1146.5 422.496C906 505.511 985 725.99 855.5 878.5C742.448 1011.64 598 1022.01 412 1042.51C245.601 1060.85 44.4999 1020.01 2.49991 795.019Z"
+          fill={color}
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default SponsorStoneBlob;
