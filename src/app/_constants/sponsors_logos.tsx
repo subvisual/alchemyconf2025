4e99ff961fea31e9f766c6bd6d8a266e7ef2e943
josevazf <josevaz.fco@gmail.com>
2025-03-20 19:00:38 +0000

    Add Alembic

--- src/app/_constants/sponsors_logos.tsx
+++ src/app/_constants/sponsors_logos.tsx
@@ -7,6 +7,7 @@ import erlangLogo from "@/assets/images/erlang_logo.svg";
 import yariLogo from "@/assets/images/yari_logo.png";
 import maerskLogo from "@/assets/images/maersk_logo.png";
 import coverflexLogo from "@/assets/images/coverflex_logo.png";
+import alembicLogo from "@/assets/images/alembic_logo.svg";
 
 const sponsorLogos = {
   remoteLogo,
@@ -18,6 +19,7 @@ const sponsorLogos = {
   yariLogo,
   maerskLogo,
   coverflexLogo,
+  alembicLogo,
 } as const;
 
 export default sponsorLogos;
